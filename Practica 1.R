01fd903d752b49c738172bceeb78243ec9304a6d
Ruley99 <ruleymorac@gmail.com>
2021-06-14 20:37:40 -0600

    Create Practica 1.R
    Continuacion Practica 1

---
+++ Practica 1.R
@@ -0,0 +1,31 @@
+LadoA = 3
+LadoB = 4
+
+Hipotenusa = sqrt((LadoA ^ 2) + (LadoB ^ 2))
+
+Hipotenusa
+------------------------------------------------------
+edad = 67
+  if(edad>=65){
+    print("Es adulto mayor")
+  }else{
+    print("No es adulto mayor")
+  }
+------------------------------------------------------
+var1 = 18
+var2 = "Esto es un string"
+var3 = 2L
+------------------------------------------------------
+VectorEnteros <-c (1:13)
+VectorEnteros
+
+vectorMeses=c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Setiembre","Octubre","Noviembre","Diciembre")
+vectorMeses
+
+
+
+v1=c("Shrek","MIB","Avengers")
+v2=c(7.5,8.5,9)
+v3=c(TRUE,FALSE,FALSE)
+concatenado<-c(v1,v2,v3)
+concatenado
