0e19bbc03a79e97deaf408ad2813718184b549d2
Ruley99 <ruleymorac@gmail.com>
2021-06-14 20:39:44 -0600

    Update Practica 1.R
    Update

--- Practica 1.R
+++ Practica 1.R
@@ -19,13 +19,34 @@ var3 = 2L
 VectorEnteros <-c (1:13)
 VectorEnteros
 
-vectorMeses=c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Setiembre","Octubre","Noviembre","Diciembre")
+vectorMeses = c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Setiembre","Octubre","Noviembre","Diciembre")
 vectorMeses
 
 
 
-v1=c("Shrek","MIB","Avengers")
-v2=c(7.5,8.5,9)
-v3=c(TRUE,FALSE,FALSE)
-concatenado<-c(v1,v2,v3)
+v1 = c("Shrek","MIB","Avengers")
+v2 = c(7.5,8.5,9)
+v3 = c(TRUE,FALSE,FALSE)
+concatenado <- c(v1,v2,v3)
 concatenado
+
+-----------------------------------------------------
+x <- c(8, 17, 23, 93, - 20, 13, 55, 29, - 84)
+length(x)
+min(x)
+max(x)
+sum(x)
+mean(x)
+median(x)
+sort(x)
+plot(x)
+unique(x)
+which.max(x)
+which.min(x)
+sqrt (14)
+-------------------------------------------------------
+vector8 <-c (1:100)
+vector8
+-------------------------------------------------------
+vector9 <- seq(0,100,10)
+vector9
